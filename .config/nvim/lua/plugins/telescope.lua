b1206c02f166416cf74583b7f5d65461a6f3a52e
StepaniaH <StepaniaH0907@gmail.com>
2024-12-21 23:57:40 +0800

    Finish init lsp-configuration of neovim.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,29 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  tag = '0.1.8',
-  config = function()
-    local builtin = require("telescope.builtin")
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    tag = '0.1.8',
+    config = function()
+      local builtin = require("telescope.builtin")
 
-    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-  end
+      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    end
+  },
+
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    -- This is your opts table
+    config = function ()
+      require("telescope").setup ({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      })
+      require("telescope").load_extension("ui-select")
+    end
+  },
 }
