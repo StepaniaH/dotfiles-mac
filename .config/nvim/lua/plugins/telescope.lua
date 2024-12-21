a38336eca160bbde49a386285614bd88c1861d2f
StepaniaH <StepaniaH0907@gmail.com>
2024-12-21 22:30:43 +0800

    Unimportant.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
   'nvim-telescope/telescope.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
   tag = '0.1.8',
   config = function()
     local builtin = require("telescope.builtin")
