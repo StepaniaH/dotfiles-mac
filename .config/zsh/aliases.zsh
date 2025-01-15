8967ff02b1d0603978abbb52d839cdae5f6fe798
StepaniaH <StepaniaH0907@gmail.com>
2025-01-15 21:14:37 +0800

    Add some urls in newsboat but also import lots of bugs, leading it keeps crushing

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,11 +1,12 @@
 # All aliases of zsh
 
 alias ls='ls -G'                                       # list with colors
-alias ll='eza -lah'                                     # list with colors and long format
+alias ll='eza -lah'                                    # list with colors and long format
 alias c='clear'                                        # clear terminal
 alias h='history'                                      # show history
 alias g='lazygit'                                      # git gui Lazygit
 alias n='neofetch'                                     # show neofetch
 alias bu='brew update && brew upgrade && brew cleanup' # update brew and upgrade, cleanup
 alias vim='nvim', vi='nvim', v='nvim'                  # neovim 
-alias cal='calcurse'
+alias cal='calcurse'                                   # calcurse
+alias news='newsboat'                                  # newsboat
