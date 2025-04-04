29d4dc22b28582685b50bf6ea43ad0bfdc5013c6
StepaniaH <StepaniaH0907@gmail.com>
2025-04-04 21:37:49 +0800

    feat(emacs): some new features and plugins of emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,10 +16,22 @@
   (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-;; (require 'init-benchmarking) ;; Measure startup time
 
 (defconst *spell-check-support-enable* nil) ;; Enable with t if you prefer
 (defconst *is-a-mac* (eq system-type 'darwin))
+
+;; 将自定义设置保存到单独的文件
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+;; 如果文件不存在则创建它
+(unless (file-exists-p custom-file)
+  (write-region "" nil custom-file))
+;; 加载自定义文件
+(load custom-file)
+
+;; Package Management
+(eval-when-compile
+ (require 'use-package))
+
 
 ;; Adjust garbage collection threshold for early startup (see use of gcmh below)
 (setq gc-cons-threshold (* 128 1024 1024))
@@ -29,18 +41,59 @@
 (setq read-process-output-max (* 4 1024 1024))
 (setq process-adaptive-read-buffering nil)
 
-;; Initial configuration:
-(require 'my-editor-config)
+;; options:
+(setq confirm-kill-emacs #'yes-or-no-p)
+(electric-pair-mode t)
+(add-hook 'prog-mode-hook #'show-paren-mode)
+(column-number-mode t)
+(global-auto-revert-mode t)
+(delete-selection-mode t)
+(setq inhibit-startup-message t)
+(setq make-backup-files nil)
+(add-hook 'prog-mode-hook #'hs-minor-mode)
+(global-display-line-numbers-mode 1)
+(tool-bar-mode -1)
+(when (display-graphic-p) (toggle-scroll-bar -1))
+(savehist-mode 1)
+(setq display-line-numbers-type 'relative)
 
 
 
 
+;;; Key bindings:
+; (global-set-key (kbd "RET") 'newline-and-indent)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 
 
 
+;; Plugins:
+
+(use-package counsel
+  :ensure t)
+(use-package ivy
+ :ensure t
+ :init
+ (ivy-mode 1)
+ (counsel-mode 1)
+ :config
+ (setq ivy-use-virtual-buffers t)
+ (setq search-default-mode #'char-fold-to-regexp)
+ (setq ivy-count-format "(%d/%d) ")
+ :bind
+ (("C-s" . 'swiper)
+  ("C-x b" . 'ivy-switch-buffer)
+  ("C-c v" . 'ivy-push-view)
+  ("C-c s" . 'ivy-switch-view)
+  ("C-c V" . 'ivy-pop-view)
+  ("C-x C-@" . 'counsel-mark-ring)
+  ("C-x C-SPC" . 'counsel-mark-ring)
+  :map minibuffer-local-map
+  ("C-r" . counsel-minibuffer-history)))
 
 
 
-
-(require 'hello)
+(provide 'init)
 ;;; init.el ends here
