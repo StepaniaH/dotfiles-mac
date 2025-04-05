28ed86d658bcd4fa6943ad31fadee88898ff23f7
StepaniaH <StepaniaH0907@gmail.com>
2025-04-05 21:49:39 +0800

    feat(emacs): add some new features and plugins for emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -57,6 +57,12 @@
 (savehist-mode 1)
 (setq display-line-numbers-type 'relative)
 
+(set-face-attribute 'default nil
+                    :family "Hack Nerd Font Mono"
+                    :height 150
+                    :weight 'normal
+                    :width 'normal)
+
 
 
 
@@ -68,7 +74,7 @@
 (package-initialize)
 
 
-
+(which-key-mode 1)
 ;; Plugins:
 
 (use-package counsel
@@ -93,6 +99,128 @@
   :map minibuffer-local-map
   ("C-r" . counsel-minibuffer-history)))
 
+(use-package hydra
+ :ensure t)
+
+(use-package use-package-hydra
+ :ensure t
+ :after hydra)
+
+
+(use-package amx
+ :ensure t
+ :init (amx-mode))
+
+(use-package ace-window
+ :ensure t
+ :bind (("C-x o" . 'ace-window)))
+
+(use-package mwim
+ :ensure t
+ :bind
+ ("C-a" . mwim-beginning-of-code-or-line)
+ ("C-e" . mwim-end-of-code-or-line))
+
+(use-package undo-tree
+ :ensure t
+ :init (global-undo-tree-mode)
+ :custom
+ (undo-tree-auto-save-history nil)
+ :after hydra
+ :bind ("C-x C-h u" . hydra-undo-tree/body)
+ :hydra (hydra-undo-tree (:hint nil)
+ "
+ _p_: undo _n_: redo _s_: save _l_: load  "
+ ("p"  undo-tree-undo)
+ ("n"  undo-tree-redo)
+ ("s"  undo-tree-save-history)
+ ("l"  undo-tree-load-history)
+ ("u"  undo-tree-visualize "visualize" :color blue)
+ ("q"  nil "quit" :color blue)))			 
+
+(use-package catppuccin-theme
+  :ensure t
+  :init
+  (load-theme 'catppuccin :no-confirm)
+  (setq catppuccin-flavor 'mocha)
+  (catppuccin-reload))
+
+(global-set-key (kbd "C-j") nil) 
+(use-package avy
+ :ensure t
+ :bind
+ (("C-j C-SPC" . avy-goto-char-timer)))
+
+(use-package marginalia
+ :ensure t
+ :init (marginalia-mode)
+ :bind (:map minibuffer-local-map
+     ("M-A" . marginalia-cycle)))
+
+(use-package multiple-cursors
+ :ensure t
+ :after hydra
+ :bind
+ (("C-x C-h m" . hydra-multiple-cursors/body)
+  ("C-S-<mouse-1>" . mc/toggle-cursor-on-click))
+ :hydra
+ (hydra-multiple-cursors
+  (:hint nil)
+  "
+Up^^       Down^^      Miscellaneous      % 2(mc/num-cursors) cursor%s(if (> (mc/num-cursors) 1) \"s\" \"\")
+------------------------------------------------------------------
+ [_p_]  Prev   [_n_]  Next   [_l_] Edit lines [_0_] Insert numbers
+ [_P_]  Skip   [_N_]  Skip   [_a_] Mark all  [_A_] Insert letters
+ [_M-p_] Unmark  [_M-n_] Unmark  [_s_] Search   [_q_] Quit
+ [_|_] Align with input CHAR    [Click] Cursor at point"
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("|" mc/vertical-align)
+  ("s" mc/mark-all-in-region-regexp :exit t)
+  ("0" mc/insert-numbers :exit t)
+  ("A" mc/insert-letters :exit t)
+  ("<mouse-1>" mc/add-cursor-on-click)
+  ;; Help with click recognition in this hydra
+  ("<down-mouse-1>" ignore)
+  ("<drag-mouse-1>" ignore)
+  ("q" nil)))
+
+(use-package dashboard
+ :ensure t
+ :config
+ (setq dashboard-banner-logo-title "Welcome to Emacs!") ;; 个性签名，随读者喜好设置
+ ;; (setq dashboard-projects-backend 'projectile) ;; 读者可以暂时注释掉这一行，等安装了 projectile 后再使用
+ (setq dashboard-startup-banner 'official) ;; 也可以自定义图片
+ (setq dashboard-items '((recents . 5)  ;; 显示多少个最近文件
+  (bookmarks . 5) ;; 显示多少个最近书签
+  (projects . 10))) ;; 显示多少个最近项目
+ (dashboard-setup-startup-hook))
+
+(use-package tiny
+ :ensure t
+ ;; 可选绑定快捷键，笔者个人感觉不绑定快捷键也无妨
+ :bind
+ ("C-;" . tiny-expand))
+
+(use-package wakatime-mode
+  :ensure t
+  :config
+  (global-wakatime-mode))
+
+(use-package highlight-symbol
+ :ensure t
+ :init (highlight-symbol-mode)
+ :bind ("<f3>" . highlight-symbol)) ;; 按下 F3 键就可高亮当前符号
+
+(use-package rainbow-delimiters
+ :ensure t
+ :hook (prog-mode . rainbow-delimiters-mode))
 
 
 (provide 'init)
